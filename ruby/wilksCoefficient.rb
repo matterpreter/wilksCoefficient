680cc285457dc9e5004ebbc4b8f6c843995718ff
mchandx <matt@cubalo.com>
2015-04-22 16:18:43 -0400

    Syntax consitency

--- ruby/wilksCoefficient.rb
+++ ruby/wilksCoefficient.rb
@@ -9,7 +9,7 @@ end
 #Calculate Wilks for men using pounds.
 def wilksMensLB(bodyweight, total):
     bodyweightLBS = bodyweight*0.453592
-    totalLBS = total*0.453592
+    totalLBS = total * 0.453592
     wilks = totalLBS * 500 / (-216.0475144 + 16.2606339 * bodyweightLBS - 0.002388645 * bodyweightLBS**2 - 0.00113732 * bodyweightLBS**3 + 0.00000701863 * bodyweightLBS**4 - 0.00000001291 * bodyweightLBS**5)
     puts wilks
 end
@@ -23,7 +23,7 @@ end
 #Calculate Wilks for women using pounds.
 def wilksWomensLB(bodyweight, total):
     bodyweightLBS = bodyweight*0.453592
-    totalLBS = total*0.453592
+    totalLBS = total * 0.453592
     wilks = totalLBS * 500 / (594.31747775582 - 27.23842536447 * bodyweightLBS + 0.82112226871 * bodyweightLBS**2 - 0.00930733913 * bodyweightLBS**3 + 0.00004731582 * bodyweightLBS**4 - 0.00000009054 * bodyweightLBS**5)
     puts wilks
 end
